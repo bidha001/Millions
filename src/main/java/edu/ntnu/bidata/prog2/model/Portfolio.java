6ff274fe27c4b3c68a348cde8ca6c96e3fd362ff
biritdhungana <binitd@stud.ntnu.no>
2026-02-10 15:35:28 +0100

    Added portfolio midel with basic share management

--- src/main/java/edu/ntnu/bidata/prog2/model/Portfolio.java
+++ src/main/java/edu/ntnu/bidata/prog2/model/Portfolio.java
@@ -1,4 +1,28 @@
 package edu.ntnu.bidata.prog2.model;
 
+import java.util.List;
+
 public class Portfolio {
+    private final List<Share> shares;
+
+    public Portfolio() {
+        this.shares = shares;
+    }
+
+    public List<Share> getShares() {
+        return shares;
+    }
+
+    public void addShare(Share share) {
+        shares.add(share);
+    }
+
+    public void removeShare(Share share) {
+        shares.remove(share);
+    }
+
+    public boolean containsShare(Share share) {
+        return shares.contains(share);
+    }
+
 }
