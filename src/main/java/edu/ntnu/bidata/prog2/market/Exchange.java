76c6a67aa4579518ba64dca776c3c8573acbaf94
biritdhungana <binitd@stud.ntnu.no>
2026-02-10 16:24:32 +0100

    added exchange with basic stock lookup and search

--- src/main/java/edu/ntnu/bidata/prog2/market/Exchange.java
+++ src/main/java/edu/ntnu/bidata/prog2/market/Exchange.java
@@ -1,4 +1,43 @@
 package edu.ntnu.bidata.prog2.market;
 
+import edu.ntnu.bidata.prog2.model.Stock;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Exchange {
+    private final String name;
+    private final Map<String, Stock> stocks;
+    private int week;
+
+    public Exchange(String name, Map<String, Stock> stocks) {
+        this.name = name;
+        this.stocks = stocks;
+        this.week = 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public Stock getStock(String symbol) {
+        return stocks.get(symbol);
+    }
+
+    public List<Stock> findStocks(String search){
+        List<Stock> result = new ArrayList<>();
+
+        for (Stock stock : stocks.values()) {
+            if (stock.getSymbol().contains(search) ||
+                    stock.getCompany().contains(search)){
+                result.add(stock);
+            }
+        }
+        return result;
+    }
 }
