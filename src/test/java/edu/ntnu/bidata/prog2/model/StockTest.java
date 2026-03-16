7f8c709cd3260bde79d4128d63776cc2f89928e0
biritdhungana <binitd@stud.ntnu.no>
2026-03-17 00:23:48 +0100

    Added getLatestPriceChange and historicalPrices test in stock class

--- src/test/java/edu/ntnu/bidata/prog2/model/StockTest.java
+++ src/test/java/edu/ntnu/bidata/prog2/model/StockTest.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StockTest {
+    /**
+     * Tests that the constructor of the Stock class correctly stores the company name, stock symbol, and initial price.
+     */
     @Test
     public void ifConstructorStoresValueCorrectly() {
         Stock stock = new Stock("Apple", "AAPL", new BigDecimal("150"));
@@ -16,6 +19,9 @@ public class StockTest {
         assertEquals(new BigDecimal("150"), stock.getSalesPrice());
     }
 
+    /**
+     * Tests that the addNewSalesPrice method correctly updates the latest price of the stock.
+     */
     @Test
     public void addNewSalesPriceUpdatesLatestPrice() {
 
@@ -26,6 +32,20 @@ public class StockTest {
         assertEquals(new BigDecimal("200"), stock.getSalesPrice());
     }
 
+    /**
+     * Tests that the getHighestPrice method returns the correct highest price from the list of prices.
+     */
+    @Test
+    public void getHighestPriceWithSinglePrice() {
+
+        Stock stock = new Stock("Apple", "AAPL", new BigDecimal("150"));
+
+        assertEquals(new BigDecimal("150"), stock.getHighestPrice());
+    }
+
+    /**
+     * Tests that the getHighestPrice method returns the correct highest price when multiple prices have been added.
+     */
     @Test
     public void getHighestPriceReturnsCorrectValue() {
 
@@ -38,6 +58,9 @@ public class StockTest {
         assertEquals(new BigDecimal("200"), stock.getHighestPrice());
     }
 
+    /**
+     * Tests that the getLowestPrice method returns the correct lowest price from the list of prices.
+     */
     @Test
     public void getLowestPriceReturnsCorrectValue() {
 
@@ -50,6 +73,9 @@ public class StockTest {
         assertEquals(new BigDecimal("120"), stock.getLowestPrice());
     }
 
+    /**
+     * Tests that the getLowestPrice method returns the correct lowest price when only a single price is present.
+     */
     @Test
     public void getLowestPriceWithSinglePrice() {
 
@@ -58,6 +84,9 @@ public class StockTest {
         assertEquals(new BigDecimal("150"), stock.getLowestPrice());
     }
 
+    /**
+     * Tests that the getLatestPriceChange method returns the correct price difference between the last two prices in the list.
+     */
     @Test
     public void getLatestPriceChangeReturnsCorrectDifference() {
 
@@ -68,11 +97,28 @@ public class StockTest {
         assertEquals(new BigDecimal("30"), stock.getLatestPriceChange());
     }
 
+    /**
+     * Tests that the getLatestPriceChange method returns BigDecimal. ZERO when there are fewer than 2 prices in the list.
+     */
     @Test
     public void getLatestPriceChangeWithSinglePriceReturnsZero() {
 
         Stock stock = new Stock("Apple", "AAPL", new BigDecimal("150"));
 
         assertEquals(BigDecimal.ZERO, stock.getLatestPriceChange());
+    }
+
+    /**
+     * Tests that the getHistoricalPrices method returns a list containing all the prices that have been added to the stock.
+     */
+    @Test
+    public void getHistoricalPricesReturnsAllPrices() {
+
+        Stock stock = new Stock("Apple", "AAPL", new BigDecimal("150"));
+
+        stock.addNewSalesPrice(new BigDecimal("180"));
+        stock.addNewSalesPrice(new BigDecimal("200"));
+
+        assertEquals(3, stock.getHistoricalPrices().size());
     }
 }
