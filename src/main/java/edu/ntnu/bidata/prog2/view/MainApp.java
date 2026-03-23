61a02a9d2cc3ff99386feca5eb7d1e00d3425823
biritdhungana <binitd@stud.ntnu.no>
2026-03-23 20:55:06 +0100

    Add top and left bars with labels to MainApp layout

--- src/main/java/edu/ntnu/bidata/prog2/view/MainApp.java
+++ src/main/java/edu/ntnu/bidata/prog2/view/MainApp.java
@@ -2,7 +2,10 @@ package edu.ntnu.bidata.prog2.view;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -10,6 +13,31 @@ public class MainApp extends Application {
     public void start(Stage stage){
         // Root layout for the application.
         BorderPane root = new BorderPane();
+
+        // -----Top Bar-----
+        HBox topBar = new HBox();
+
+        // spacing between elements
+        topBar.setSpacing(20);
+
+        // labels for the top bar
+        Label name = new Label("Name: ");
+        Label money = new Label("Money: ");
+        Label netWorth = new Label("Net Worth: ");
+        Label status = new Label("Status: ");
+        Label week = new Label("Week: ");
+
+        // add all labels to the top bar
+        topBar.getChildren().addAll(name, money, netWorth, status, week);
+        // place at top
+        root.setTop(topBar);
+
+        // -----Left Bar-----
+        VBox left = new VBox();
+        Label stocksTitle = new Label("Stocks: ");
+        left.getChildren().add(stocksTitle);
+        root.setLeft(left);
+
 
         // Scene
         Scene scene = new Scene(root, 1000, 700);
